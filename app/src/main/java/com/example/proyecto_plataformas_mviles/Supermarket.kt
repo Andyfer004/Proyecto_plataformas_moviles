0bfce69e50076ba57648738151f01bf67316edb5
SergioAle210 <98841969+SergioAle210@users.noreply.github.com>
2023-09-11 20:48:51 -0600

    Se agregó un diseñito a la segunda pantalla.

--- app/src/main/java/com/example/proyecto_plataformas_mviles/Supermarket.kt
+++ app/src/main/java/com/example/proyecto_plataformas_mviles/Supermarket.kt
@@ -4,10 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.proyecto_plataformas_mviles.ui.theme.Proyecto_Plataformas_móvilesTheme
 import com.example.proyecto_plataformas_mviles.ui.theme.colorb
 
@@ -18,7 +25,7 @@ class Supermarket : ComponentActivity() {
             Proyecto_Plataformas_móvilesTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize() ,color = colorb ) {
-                    Greeting2("")
+                    Greeting2()
                 }
             }
         }
@@ -27,18 +34,23 @@ class Supermarket : ComponentActivity() {
 
 
 @Composable
-fun Greeting2(name: String, modifier: Modifier = Modifier) {
+fun Greeting2() {
+        Text(
+            text = "Supermarket",
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp)
+        )
+
 }
-
-
-
-
-
 
 @Preview(showBackground = true)
 @Composable
 fun Greeting2Preview() {
     Proyecto_Plataformas_móvilesTheme {
-        Greeting2("")
+        Greeting2()
     }
 }
